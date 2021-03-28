5383e9430b1339724a4327d933f7ffb5eb827003
Parth <pcs003@ucsd.edu>
2021-03-28 16:48:49 -0400

    Attempt 2 to fix heroku

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ const db = require('./config/keys').mongoURI;
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
@@ -24,13 +31,6 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
-}
 
 app.get("/", (req, res) => res.send("Hello World"));
 app.use("/api/users", users);
